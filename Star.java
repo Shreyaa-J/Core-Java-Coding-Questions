6daefe9bb5b31b4567418a1c31fdfde356348904
Shreya Jadhav <142742335+Shreyaa-J@users.noreply.github.com>
2025-07-20 22:46:55 +0530

    Add files via upload

---
+++ Star.java
@@ -0,0 +1,24 @@
+
+public class Star {
+	
+	public static void main(String[] args) {
+		int j,k;
+		for(int i=1;i<=5;i++) {
+			for( j=1;j<=i;j++) {
+				//System.out.print("  ");
+				System.out.print(" * ");
+				/*for(k=j;k<=i;k++) {
+					if(k<=i) {
+					System.out.print(" * ");
+					}
+					else {
+						System.out.println(" ");
+					}
+				}*/
+				
+			}
+			 System.out.println();
+		}
+	}
+
+}
